ef16cd3c6319ef71e0abc370a85295fc0dbff122
Paruchuri sivaprasad <101115356+paruchurisivaprasad@users.noreply.github.com>
2022-07-16 23:33:56 +0530

    sequelize model shoping app

---
+++ sequelizeDB/controllers/admin.js
@@ -0,0 +1,100 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+    Product.create({
+      title:title,
+      price:price,
+      imageUrl:imageUrl,
+      description:description
+    }).then(result=>{
+      console.log(" product created ");
+      res.redirect('/admin/products')
+
+    }).catch(err=>{
+      console.log(err);
+    });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findAll({where:{id:prodId}})
+  .then(product => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product[0]
+    });
+  })
+  .catch(err=>console.log(err));
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  Product.findAll({where:{id:prodId}})
+  .then(product=>{
+    product[0].title=updatedTitle;
+    product[0].price=updatedPrice;
+    product[0].imageUrl=updatedImageUrl;
+    product[0].description=updatedDesc;
+   return  product[0].save();
+
+  })
+  .then(result=>{
+    console.log('post updated'); 
+     res.redirect('/admin/products');
+
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll().then(products=>{
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findAll({where:{id:prodId}})
+  .then(product=>{
+    return product[0].destroy();
+  })
+  .then(result=>{
+    console.log('product destroyed');
+    res.redirect('/admin/products');
+
+  })
+  .catch(err=>{
+    console.log(err);
+  })
+};
